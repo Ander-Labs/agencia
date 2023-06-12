e0a2cd50ec6e54a7d5eccd53910794aec923f411
ANDER <andersonruizr2@gmail.com>
2023-06-12 09:32:57 -0500

    Update layaut.jsx

--- src/components/layaut.jsx
+++ src/components/layaut.jsx
@@ -12,6 +12,7 @@ export default function Layout({ children, title, description, image }) {
         <Head>
             <title>{title}</title>
             <meta name='description' content={description} />
+            <link rel="icon" href="/favicon.ico" sizes="any" />
 
             {/* Etiquetas Open Graph */}
         <meta property="og:title" content={title} />
